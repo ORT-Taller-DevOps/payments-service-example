783d17db29e1ec854f4e0e5d15a4685e69a1531b
Thiago Schreck <thiago.schreck1@gmail.com>
2024-07-13 17:28:55 -0300

    (feature) Updated scaling config

--- terraform/main.tf
+++ terraform/main.tf
@@ -43,9 +43,9 @@ resource "aws_eks_node_group" "node_group" {
     env = var.environment
   }
   scaling_config {
-    desired_size = 2
-    max_size     = 2
-    min_size     = 2
+    desired_size = 1
+    max_size     = 1
+    min_size     = 1
   }
   update_config {
     max_unavailable = 1
