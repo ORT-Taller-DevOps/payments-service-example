2e265b7449c2e02ad06d047fbad50863a1dfbce8
Thiago Schreck <thiago.schreck1@gmail.com>
2024-07-13 21:33:20 -0300

    (feature) Updating scaling config and docker image tag selection

--- terraform/main.tf
+++ terraform/main.tf
@@ -43,9 +43,9 @@ resource "aws_eks_node_group" "node_group" {
     env = var.environment
   }
   scaling_config {
-    desired_size = 1
-    max_size     = 1
-    min_size     = 1
+    desired_size = 3
+    max_size     = 5
+    min_size     = 3
   }
   update_config {
     max_unavailable = 1
