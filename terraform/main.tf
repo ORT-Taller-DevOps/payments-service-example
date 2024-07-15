db0a14f20b3b6757ec8f708d898c1d27685db0a7
Thiago Schreck <thiago.schreck1@gmail.com>
2024-07-15 00:29:46 -0300

    (feature) Upped the max size so the master node fits

--- terraform/main.tf
+++ terraform/main.tf
@@ -44,7 +44,7 @@ resource "aws_eks_node_group" "node_group" {
   }
   scaling_config {
     desired_size = 2
-    max_size     = 2
+    max_size     = 3
     min_size     = 1
   }
   update_config {
